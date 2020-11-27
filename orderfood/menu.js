23624407f4a9aa393e2019823203b92547d1b0e6
謝愛佳 <36994634+victoriatjia@users.noreply.github.com>
2020-11-27 09:29:47 +0800

    Add files via upload

---
+++ orderfood/menu.js
@@ -0,0 +1,7 @@
+var menu= new Array(24)
+menu=[["menu/1.jpg",80, "Kit Kat Sushi Bars"],["menu/2.jpg",120,"Sushi Sen Fusion Roll"],["menu/3.jpg",300,"The Una Don and Sakana Set"],["menu/4.jpg",80,"Rising Sun Veggies"],["menu/5.jpg",120,"Double Shrimp Roll"],["menu/6.jpg",120,"Smoked Chicken"],["menu/7.jpg",120, "The Yum Yum Tree"],["menu/8.jpg",150,"Big Wong XL"],["menu/9.jpg",150,"Beef Sesame B&W"],["menu/10.jpg",160,"Spider Hosomaki Avocado Tempura Roll"],["menu/11.jpg",160,"Tiger Roll"],["menu/12.jpg",80,"The Simplicity"],["menu/13.jpg",100,"The Vegetarian Maki Platter"],["menu/14.jpg",160,"Shiny Smoked Salmon"],["menu/15.jpg",130,"The Majestic Roll"],["menu/16.jpg",100,"Tama Go"],["menu/17.jpg",160,"Peking Duck Maki"],["menu/18.jpg",170,"Assorted Sushi"],["menu/19.jpg",150,"Authentic California Roll"],["menu/20.jpg",160,"Asian Barbeque Scottish Salmon"],["menu/21.jpg",160,"Billy Beach Sushi"],["menu/22.jpg",100,"Little Greeny Veggies"],["menu/23.jpg",130,"Rainbow Rolls"],["menu/24.jpg",100,"Cabbage Strolls"]]
+
+var fixedorder= new Array()
+fixedorder=[0]
+
+//http://203.64.84.113/STUpload/StFiles/305/20180405-1932/online%20web/orderfood.html
